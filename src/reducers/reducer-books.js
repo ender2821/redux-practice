c09e3eca6f4678dcd4bd56300145b47aebf14237
Josh Jensen <ender2821@yahoo.com>
2018-02-14 22:07:46 -0600

    created boilerplate, added some mock data for addition of redux, and redux containers

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'The 48 Laws of Power'},
+		{title: 'The art of Seduction'},
+		{title: 'Zen and the art of Motorcycle Maintenance'},
+		{title: 'Mastery'}
+	]
+}
