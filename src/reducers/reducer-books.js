9f509a78f214ba310807af29359bef4267ec524e
Josh Jensen <design@joshjensencreative.com>
2018-02-16 13:05:50 -0600

    added priliminary action for the container on load, also added more data to show how it can easily be manipulated.

--- src/reducers/reducer-books.js
+++ src/reducers/reducer-books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{title: 'The 48 Laws of Power'},
-		{title: 'The art of Seduction'},
-		{title: 'Zen and the art of Motorcycle Maintenance'},
-		{title: 'Mastery'}
+		{title: 'The 48 Laws of Power', pages: 124},
+		{title: 'The art of Seduction', pages: 244},
+		{title: 'Zen and the art of Motorcycle Maintenance', pages: 321},
+		{title: 'Mastery', pages:257 }
 	]
 }
